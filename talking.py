79f8c030b83f3b97f189a8a6979aa5242ac7fb41
a scary lesbian <penguinstakingover@gmail.com>
2025-05-14 09:30:27 +1200

    test commit

---
+++ talking.py
@@ -0,0 +1,14 @@
+#Talking to NPC's module
+
+#dictionary, flow OF DIALOUGE OPTIONS
+dialogue = {
+    "Coral": ["hi", "ok", "free will", "bye"]
+    "Swish": ["Hey Champ", "I like the air", "Its pretty", "ok... bye?"]
+    "Lyx": ["hi...", "okay...", "ok", "free will"]
+    }
+
+#npc being currently interacted with
+npc = "Coral"
+
+def talk(npc):
+    if #might have to use if else pathways
