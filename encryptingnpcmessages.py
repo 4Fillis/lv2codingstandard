7e1d4ec165658e25fba258d6d70f719e744c47c4
illiotes <165102269+4Fillis@users.noreply.github.com>
2025-05-15 13:31:39 +1200

    Update encryptingnpcmessages.py talk half done need to do out of range

--- encryptingnpcmessages.py
+++ encryptingnpcmessages.py
@@ -1,7 +1,20 @@
-npctext = "example"
-npctext = npctext.lower()
+#name of npc being interacted with
+npcname = "Coral"
 
-converter = {
+def npctalk(npcname):
+    #npc dialogue options
+    npctext = {
+    "Coral": ["hello", "oh hi, it's you again"],
+    "Persephone": ["beware small creature, leave if you value your life", "you won't last long here"]
+    }
+    #counting number of interactions per npc
+    npcintercnt = {
+        "Coral": 0,
+        "Persephone": 0
+    }
+
+    #dictionary with english letters and npc equivilent
+    converter = {
     "a": "🎬",
     "b": "☣️",
     "c": "🎥",
@@ -28,8 +41,17 @@ converter = {
     "x": "🔪",
     "y": "🌻",
     "z": "🥀"
-}
+    }
 
-def encodemsg(npcmsg):
-    for i in range(len(npctext)):
-        print(converter[npctext[i]], end = " ")
+    #finding correct npc message
+
+    npcmsg = npctext[npcname][npcintercnt[npcname]]
+    npcmsg = npcmsg.lower()
+    #do smth if out of range
+
+
+    #turns input text into emoji translate
+    for i in range(len(npcmsg)):
+        print(converter[npcmsg[i]], end = " ")
+
+npctalk(npcname)
