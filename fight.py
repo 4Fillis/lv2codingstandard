31ecd13ebe43da1aa0bcf5db68654fe4e76f3376
illiotes <165102269+4Fillis@users.noreply.github.com>
2025-05-16 11:01:02 +1200

    add fight file

---
+++ fight.py
@@ -0,0 +1,14 @@
+#function to fight whatever you're gonna fight
+
+#ADD DICTIONARY OF ITEMS IN GAME
+
+#variables
+cinteractin = "bread"
+fighting = cinteractin
+
+#IF FIGHT CHANGE CURRENT INTERACTION TO FIGHT
+
+#DISCERN IF FIGHTING ITEM OR PERSON/HUMANOID
+
+def fight(fighting):
+    print("print")
