53d4111640bfbfa252e7c0d97c1f2fb45635dcd3
illiotes <165102269+4Fillis@users.noreply.github.com>
2025-06-08 16:10:38 +1200

    healthcheck function redone - about to integrate

--- healthcheck.py
+++ healthcheck.py
@@ -1,28 +1,47 @@
 
-hp = 10
-
+#dictionaries
+bodyparts = {
+  "left arm": "pulls out", 
+  "right arm": "scratches", 
+  "shirt": "grabs",
+  "left leg": "pulls", 
+  "right leg": "yoinks", 
+  "foot": "stamps on",
+  "mass": "grabs at",
+  "head": "yanks", 
+  "eye": "pokes",
+  "shin": "bruises"
+}
+parts_taken = ["left arm", "golden eye", "false leg", "mass", "head", "yep", "a", "b", "c" ,"d"]
+def newln():
+  print("\n")
+def death():
+    print("you have died")
+    quit()
 #function to check players hp
-def healthcheck(hp):
+def healthcheck(parts_taken, bodyparts, name):
+    health_max = len(bodyparts)
+    health = health_max - len(parts_taken)
+    health_percent = (health/health_max)*100
+    print(f"{name}, you have {health_percent}% health")
     
-    #10 is max hp, buff items could bring beyond
-    if hp >= 10:
-        print("Oh you feel fine, good in fact,"
-              " the voices in your head tells you to forge onwards")
-
-    #increments, messages based on hp %
-    elif hp >= 8:
-        print(f"you can still feel ")
-    elif hp >= 5:
-        print(f"you are.... ok .... not great ... but ok, \n"
-              "some scratches on your arm are still bleeding...")
-    elif hp >= 1:
-        print(f"you can feel you're wounds draining your life\n"
-              "the voice in your head tells you not to continue"
-              "\n....\nnot sure if it'll kill you")
+    if health_percent >= 100:
+        print("you feel: spectacular, forge onwards!")
+    #increments, messages based on health %
+    elif health_percent >= 75:
+        print(f"you are finee")
+    elif health_percent >= 50:
+        print(f"status = mehh, ok above average")
+    elif health_percent >= 25:
+        print(f"Advice: heal")
+    elif health_percent > 0:
+        print(f"you're wounds have taken a heavy toll\n"
+            "Advice: do not to continue without sufficient healing")
     else:
+        print("ERROR entity_living == false")
         print("you collapse on the ground as you're wounds prove too much to handle")
-        #INSERT RESTART FUNCTION
+        death()
 
     #INSERT OPTION TO DRINK HEALTH POTION
 
-healthcheck(hp=2)
+healthcheck(parts_taken, bodyparts, name="maria")
