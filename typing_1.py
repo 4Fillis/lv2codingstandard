472de713fcaab422afe0792bbe6eb4e142ac54e9
illiotes <165102269+4Fillis@users.noreply.github.com>
2025-06-25 09:29:34 +1200

    PATHS dictionary done

--- typing_1.py
+++ typing_1.py
@@ -32,34 +32,40 @@ al = [{0:"room"}, {1:"candy cane bathroom"}, {2:"rainbow door"}, {3:"pastry hall
 paths_dict = {
   #area   forwards                 left                right          backwards
   #room
-  al[0][0]: [(f"{al[2][2]} f"),     (f"{al[1][1]} l"),     r,                 b],
+  al[0][0]: [(f"{al[2][2]} f"),     (f"{al[1][1]} l"),     r,                  b],
   #bathroom
-  al[1][1]: [f,                     l,                     r,                 (f"{al[0][0]} b")],
+  al[1][1]: [f,                     l,                     r,                  (f"{al[0][0]} b")],
   #rainbow door
-  al[2][2]: [f,                     (f"{al[3][3]} l"),    (f"{al[8][8]} l"),  (f"{al[0][0]} b")],
+  al[2][2]: [f,                     (f"{al[3][3]} l"),    (f"{al[8][8]} l"),   (f"{al[0][0]} b")],
   #LEFT from room hallway
-  al[3][3]: [(f"{al[4][4]} f"),     l,                     r,                 (f"{al[2][2]} b")],
+  al[3][3]: [(f"{al[4][4]} f"),     l,                     r,                  (f"{al[2][2]} b")],
   #RIGHT from room hallway
-  al[8][8]: [(f"{al[10][10]} f"),   l,                    (f"{al[9][9]} l"),  (f"{al[3][3]} b")],
+  al[8][8]: [(f"{al[10][10]} f"),   l,                    (f"{al[9][9]} l"),   (f"{al[3][3]} b")],
   #little kids room
-  al[11][11]:[f,                    (f"{al[10][10]} l"),   r,                 (f"{al[10][10]} b")],
+  al[11][11]:[f,                    (f"{al[10][10]} l"),   r,                  (f"{al[10][10]} b")],
   #Elevator
-  al[12][12]:[(f"{al[13][13]} f"),  (f"{al[17][17]} l"),   r,                 b],
+  al[12][12]:[(f"{al[13][13]} f"),  (f"{al[17][17]} l"),   r,                  b],
   #Admin [facing admin desk/entering building perspective]
-  al[17][17]:[f,                    (f"{al[12][12]} l"),   r,                 (f"{al[18][18]} b")],
+  al[17][17]:[f,                    (f"{al[12][12]} l"),   r,                  (f"{al[18][18]} b")],
   #Waitig area
-  al[18][18]:[f,                    (f"{al[19][19]} l"),   r,                 (f"{al[17][17]} b")],
+  al[18][18]:[f,                    (f"{al[19][19]} l"),   r,                  (f"{al[17][17]} b")],
   #Outside
-  al[19][19]:[(f"{al[20][20]} f"),  l,                     r,                 (f"{al[18][18]} b")],
+  al[19][19]:[(f"{al[20][20]} f"),  l,                     r,                  (f"{al[18][18]} b")],
   #Cookiehouse
-  al[13][13]:[(f"{al[14][14]} f"),  l,                     r,                 (f"{al[12][12]} b")],
+  al[13][13]:[(f"{al[14][14]} f"),  l,                     r,                  (f"{al[12][12]} b")],
   #Gingerbread steps
-  al[14][14]:[f,                    (f"{al[15][15]} l"),   r,                 (f"{al[13][13]} b")],
+  al[14][14]:[f,                    (f"{al[15][15]} l"),   r,                  (f"{al[13][13]} b")],
   #Candy Carpark
-  al[2][2]: [(f"{al[20][20]} f"),   l,                     r,                 (f"{al[14][14]} b")],
+  al[2][2]: [(f"{al[20][20]} f"),   l,                     r,                  (f"{al[14][14]} b")],
   #left from rainbow door/room path
   #Cookie stairs
-  al[4][4]: [(f"{al[5][5]} f"),     l,                     r,                 (f"{al[3][3]} b")]
+  al[4][4]: [(f"{al[5][5]} f"),     l,                     r,                  (f"{al[3][3]} b")],
+  #seaweed power generator
+  al[5][5]: [f,                     l,                    (f"{al[6][6]} r"),   (f"{al[4][4]} b")],
+  #side door
+  al[6][6]: [(f"{al[7][7]} f"),     l,                    r,                   (f"{al[5][5]} b")],
+  #Fudge carpark
+  al[8][8]: [f,                     l,                    (f"{al[20][20]} l"), (f"{al[6][6]} b")],
 }
 
 for key, value in paths_dict.items():
